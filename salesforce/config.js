b616edb1c6bc652cae5a520214c6ea0e0345ee93
justBanks <Darryl.Banks@gmail.com>
2020-03-28 02:07:50 -0700

    connect to heroku

---
+++ salesforce/config.js
@@ -0,0 +1,9 @@
+module.exports =  {
+    // instance_url: "https://test.salesforce.com/services",
+    // securityToken: (String:Optional) The security token to be used for the connection (single-user mode only).
+    connected_app:  "https://nodesvr.herokuapp.com",
+    client_id:      "3MVG9oZtFCVWuSwPENXV_adRl8Q1TtPA9SFpGB0c6SlXpnzMJptTplD0uA..426Xg_0xPPsOYCr_5wCRWFsz6",
+    client_secret:  "56611D9CE2BABC2009BF31B57D81E0A9AAE36F21EBD61CBC23ED71609FDAF8CF",
+    username:       "dlab.usr@gmail.com",
+    password:       "pw8chars",
+}
