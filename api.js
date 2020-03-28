9688ee8bb4ee69c1162c267b39cfdaf9bc1f0644
justBanks <Darryl.Banks@gmail.com>
2020-03-28 01:56:19 -0700

    refactor

--- api.js
+++ api.js
@@ -5,11 +5,22 @@ const leads = require('./salesforce/leads')
 const app = express()
 app.use(express.json({ type: 'application/json' })) // this type: is required by body-parser
 
-// define error-handling middleware last, after other app.use() and routes calls
+app.use(function(req, res, next){
+  if (req.method == 'POST'){
+    if(JSON.stringify(req.body) == '{}') {
+      handleError(res, 400, 'Could not read the request payload.',
+        "Make sure the request has a Content-Type header ('application/json')")
+      return
+    }
+  }
+  next()
+})
+
+// define error-handling middleware last, after other app.use() calls
 // https://expressjs.com/en/guide/error-handling.html#the-default-error-handler
 app.use(function(err, req, res, next) {
-  console.log(err.toString())
   if (res.headersSent) return next(err)
+  console.log(err.toString())
   handleError(res, 500, 'Unhandled error', err.toString())
 })
 
@@ -28,34 +39,26 @@ app.options(function(req, res) {
   res.send(204)
 })
 
-app.get('/', function (req, res) {
-  res.send({"message": "Hello, world"})
-})
+app.route('/')
+  .get(function (req, res) {
+    res.send({"message": "Hello, world"})
+  })
 
-app.get("/api/contacts", function(req, res) {
+app.route('/api/contacts')
+  .get(function(req, res) {
   // return SOQL query for the list
-})
+  })
+  .post(function(req, res) {
+    contacts.insert(req.body, res)
+  })
 
-app.post("/api/contacts", function(req, res) {
-  if(JSON.stringify(req.body) == '{}') {
-    handleError(res, 400, 'Could not read the request payload.',
-                "Make sure the request has a Content-Type header for 'application/json'")
-    return
-  }
-  contacts.insert(req.body, res)
-})
-
-app.post("/api/leads", function(req, res) {
-  if(JSON.stringify(req.body) == '{}') {
-    handleError(res, 400, 'Could not read the request payload.',
-                "Make sure the request has a Content-Type header for 'application/json'")
-    return
-  }
-  leads.insert(req.body, res)
-})
+app.route('/api/leads')
+  .post(function(req, res) {
+    leads.insert(req.body, res)
+  })
 
 //Generic error handler for use by all endpoints; Keeps this API from returning HTML error pages
 function handleError(res, statusCode, reason, message) {
   console.log("ERROR: " + reason + '\n' + message)
-  res.status(statusCode || 500).send(`${reason}\n ${message}`)
+  res.status(statusCode || 500).send(`${reason}\n ${message}\n`)
 }
